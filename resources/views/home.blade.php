dacbafcc6a3f4f22479927bf9d891e479fa2fe5c
Chris-Fancy-Pants <christo@Christophers-MacBook-Pro.local>
2018-07-06 10:10:56 +1000

    Grid update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,13 @@
                 @foreach($recipes as $recipe)
                 
                 <div id="recipe-holder-{{$recipe->id}}" class="recipe-holder-show row">
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-sm-12">
                         <a href="{{ url('recipe/' . $recipe->id) }}">{{$recipe->name}}</a>
                     </div>
-                    <div class="col-md-2">
+                    <div class="col-md-2 col-sm-6">
                         <a class="btn btn-primary" href="{{ url('recipe/edit/' . $recipe->id) }}">Edit</a>
                     </div>
-                    <div class="col-md-2">
+                    <div class="col-md-2 col-sm-6">
                         <div class="btn btn-danger" onclick="deleteRecipe({{$recipe->id}})">Delete</div>
                     </div>
                 </div>
